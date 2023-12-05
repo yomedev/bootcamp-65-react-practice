78c676e258c4ed1a71d0bf4679c9948780d9b139
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-12-05 14:46:28 +0200

    add todos

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,23 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
+import todos from './../data/todos.json';
 
 export class Todos extends Component {
+  state = {
+    todos: todos,
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    const { todos } = this.state;
+    return (
+      <Grid>
+        {todos.map(({id, text}, index) => (
+          <GridItem key={id}>
+            <Todo text={text} index={index + 1}/>
+          </GridItem>
+        ))}
+      </Grid>
+    );
   }
 }
