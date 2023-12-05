dc431e8b6e265113434e01746b4469b16c9053d9
VictorFil67 <victorfilinovych@gmail.com>
2023-12-05 16:08:40 +0200

    search todos

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,6 +5,20 @@ import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          onChange={this.props.onSearch}
+          value={this.props.search}
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -6,29 +6,43 @@ import todos from './../data/todos.json';
 export class Todos extends Component {
   state = {
     todos: todos,
+    search: '',
   };
-
+  handlerChangeSearch = e => {
+    this.setState({ search: e.target.value });
+  };
   handlerDelete = id => {
     this.setState(prevState => ({
       todos: prevState.todos.filter(el => el.id !== id),
     }));
   };
-
+  getFilteredTodos = () => {
+    return this.state.todos.filter(({ text }) =>
+      text.toLowerCase().includes(this.state.search.toLowerCase())
+    );
+  };
   render() {
     const { todos } = this.state;
+    const filteredTodos = this.getFilteredTodos();
     return (
-      <Grid>
-        {todos.map(({ id, text }, index) => (
-          <GridItem key={id}>
-            <Todo
-              id={id}
-              text={text}
-              index={index + 1}
-              onDelete={this.handlerDelete}
-            />
-          </GridItem>
-        ))}
-      </Grid>
+      <>
+        <SearchForm
+          onSearch={this.handlerChangeSearch}
+          search={this.state.search}
+        />
+        <Grid>
+          {filteredTodos.map(({ id, text }, index) => (
+            <GridItem key={id}>
+              <Todo
+                id={id}
+                text={text}
+                index={index + 1}
+                onDelete={this.handlerDelete}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </>
     );
   }
 }
